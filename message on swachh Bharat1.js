14082184e596242b1fe95d6949549b79f735b4d1
vishweshkadam <ravindra19772000@gmail.com>
2021-08-03 12:41:40 +0530

    first commit

---
+++ message on swachh Bharat1.js
@@ -0,0 +1,8 @@
+
+ function replay(){
+   window.location=("LEVEL 1.html")
+ }
+ function next(){
+  window.location=("LEVEL 2.html")
+}
+  
---
+++ message on swachh Bharat2.js
@@ -0,0 +1,8 @@
+
+ function replay(){
+   window.location=("LEVEL 2.html")
+ }
+ function next(){
+  window.location=("LEVEL 3.html")
+}
+  
